338728cafa781507d80edd86270555ae4fdad80a
Jordan Benner <jordanbenner@outlook.com>
2017-06-29 10:04:56 -0500

    Page routing
    Added browser router

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 import './App.css';
+
+const Home = () => (<h2>Home</h2>)
 
 class App extends Component {
   render(){
   return (
       <MuiThemeProvider>
+        <BrowserRouter>
+          <div>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/form">Form</Link></li>
+            </ul>
+            <Route exact path="/" component={Home}/>
+            <Route path="/form" component={MyForm}/>
+          </div>
+        </BrowserRouter>
         <div>
           <AppBar
             title="Contact App"
             iconClassNameRight="muidocs-icon-navigation-expand-more"/>
-        </div>
+          </div>
       </MuiThemeProvider>
     )
   }
