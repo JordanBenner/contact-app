293006e0250f604a36a37e10277d0f2346514000
Jordan Benner <jordanbenner@outlook.com>
2017-07-05 09:35:49 -0500

    Backend setup
    Initialized backend

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,16 @@
 import * as firebase from "firebase";
 
-
+// backend setup
 var config = {
   apiKey: "AIzaSyBRoC5-hJZf_RmAxe2QPx0ketrypq6rah8",
-  authDomain: "dcpretest.firebaseapp.com",
-  databaseURL: "https://dcpretest.firebaseio.com",
-  storageBucket: "dcpretest.appspot.com",
+   authDomain: "contact-app-862c7.firebaseapp.com",
+   databaseURL: "https://contact-app-862c7.firebaseio.com",
+   projectId: "contact-app-862c7",
+   storageBucket: "contact-app-862c7.appspot.com",
+   messagingSenderId: "1055934082544"
 };
 firebase.initializeApp(config);
+
+
 var database = firebase.database();
 export default database;
