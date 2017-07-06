c243eaeb72a350d5770bc6022bc583ada5bb89df
Jordan Benner <jordanbenner@outlook.com>
2017-07-06 09:49:38 -0500

    Adding in redux
    store.js reducer.js action.js

---
+++ src/action.js
@@ -0,0 +1,28 @@
+export function addContact (index, data) {
+
+  return {
+    type: 'ADD_CONTACT',
+    index: index,
+    data: data
+  }
+}
+
+
+export function addContact (index, data) {
+
+  return {
+    type: 'EDIT_CONTACT',
+    index: index,
+    data: data
+  }
+}
+
+
+export function addContact (index, data) {
+
+  return {
+    type: 'DELETE_CONTACT',
+    index: index,
+    data: data
+  }
+}
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux';
+import contacts from './reducers';
+
+
+var store = createStore(contacts);
+
+
+export default store;
